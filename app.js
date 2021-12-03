8adcc9927fac385cf683c6943a9b09aa8e21a376
unknown <chaudharyujjwal302@gmail.com>
2021-12-03 23:20:25 +0530

    new

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ const cors = require("cors");
 
 const app = express();
 
-app.disable('x-powered-by');
 app.use(express.static('${__dirname}/public'));
 app.use(express.json([]));
 app.use(bodyParser.urlencoded({ extended: true }));
